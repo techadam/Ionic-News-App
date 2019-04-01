8aebe0b37075f5aa254ac13e21ffe844f36164ea
Abdulrahman <abdulflezy13@gmail.com>
2019-04-01 13:54:11 +0100

    Updated dependencies

---
+++ src/app/news/news.page.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss']
+})
+export class NewsPage implements OnInit {
+  data: any;
+  page = 1;
+  constructor(private newsService: NewsService, private router: Router) {}
+
+  ngOnInit() {
+    this.newsService
+      .getData(
+        `top-headlines?sources=medical-news-today&pageSize=5&page=${
+          this.page
+        }`
+      )
+      .subscribe(data => {
+        console.log(data);
+        this.data = data;
+      });
+  }
+
+  onGoToNewsSinglePage(article) {
+    this.newsService.currentArticle = article;
+    this.router.navigate(['/news-single']);
+  }
+
+  loadMoreNews(event) {
+    this.page++;
+    console.log(event);
+    this.newsService
+      .getData(
+        `top-headlines?sources=medical-news-today&pageSize=5&page=${
+          this.page
+        }`
+      )
+      .subscribe(data => {
+        // console.log(data);
+        // this.data = data;
+        for (const article of data['articles']) {
+          this.data.articles.push(article);
+        }
+        event.target.complete();
+        console.log(this.data);
+      });
+  }
+}
